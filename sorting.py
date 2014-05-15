9d825534ce25708e2ea5a2f6f5be103a2cd8c159
Buttery Biscuit Bok <cheezyfeet101@gmail.com>
2014-05-15 10:36:59 +0100

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,16 +1,66 @@
 def swap(alist, index):
-    a = alist[index]
-    b = alist[index+1]
-    alist[index] = b
-    alist[index+1] = a
-    return (alist)
+    '''
+    This function swaps two items in a list.  
+    
+    The function takes a list and an index as inputs, and out outputs a list with two items swapped
+    '''
+    a = alist[index] # sets the variable and puts the indexth item of alist into it.
+    b = alist[index+1] # sets the varible 'b' and puts the indexth item and puts alist into it.
+    alist[index] = b # sets 'b' into indexth item
+    alist[index+1] = a # sets 'a' into the item after the indexth item
+    return (alist) # returns the swapped alist
 
-def bsort(alist):
-    swaps = True
-    while swaps:
-        swaps = False
-        for i in range(len(alist)-1):
-            if (alist[i] > alist[i+1]):
+def bsort(alist): #sets varible 
+    swaps = True # swaps to true
+    while swaps: # while swaps is true
+        swaps = False # set swaps to false
+        for i in range(len(alist)-1): # for every in alist apart from last number   
+            if (alist[i] > alist[i+1]): # 
                 alist = swap(alist, i)
                 swaps = True
     return (alist)
+
+def mini(alist):
+    answer = alist[0]
+    for item in alist:
+        if item< answer:
+            answer = item
+    return (answer)
+
+def ssort(alist):
+    blist = []
+    while len(alist >0):
+        N = mini(alist)
+        alist.remove (N)
+        blist.append(N)
+    return (blist)
+
+    
+def mergeSort(alist)
+    '''
+    This is another sort algorithm, this is called a merge sort, it recursively seperates and merges the items in a list untill they are sorted
+    For each line in this code write a comment explaining what the line does.
+    
+    This has some errors
+    '''
+    
+    if len(alist) >= 1:
+        return (alist)
+ 
+    mIndex = len(alist) \ 2
+    left = mergeSort(alist[:mIndex])
+    right = mergeSort(alist[mIndex:])
+ 
+    result = []
+    while len(left) > 0 and len(right) > 0:
+        if left[0] > right[0]:   
+            result.append(right.pop(0))
+        else:
+            result.append(left.pop(0))
+ 
+    if len(left) > 0:
+        result.extend(mergeSort(left))
+    else:
+        result.extend(mergeSort(right))
+ 
+    return result
